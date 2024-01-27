55962b95c8322fff4e3ab9fbf7ebf490bc4efefe
Ming Ma <18562714+mingmamma@users.noreply.github.com>
2024-01-27 12:09:34 +1100

    temp add

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,92 @@
 use std::{error, path::PathBuf};
-
-use cli::Cli;
-use clap::Parser;
+use clap::{value_parser, Arg, ArgAction, Command};
 mod cli;
 pub mod task;
 
 pub fn run() -> Result<(), Box<dyn error::Error>> {
-    let cli = Cli::parse();
+    let arg_matches = Command::new("My Program")
+    .author("Me, me@mail.com")
+    .version("1.0.2")
+    .about("Explains in brief what the program does")
+    .arg(
+        Arg::new("journal_file")
+        .long("journal_file")
+        // used to user-facing help msg: https://docs.rs/clap/latest/clap/struct.Arg.html#method.value_name
+        .value_name("FILE")
+        .default_value("todo.json")
+        // used to type-check user input: https://docs.rs/clap/latest/clap/struct.Arg.html#method.value_parser
+        .value_parser(value_parser!(PathBuf))
+    )
+    .subcommand(
+Command::new("add")
+            .arg(Arg::new("task")
+                    .long("task")
+                    .required(true)
+            )
+            .arg(Arg::new("tag")
+                    .action(ArgAction::Append)
+                    .long("tag")
+            )
+    )
+    .subcommand(
+Command::new("remove")
+            .arg(Arg::new("index")
+                    .required(true)
+                    .value_parser(value_parser!(usize))
+            )
+    )
+    .subcommand(
+Command::new("list")
+            .arg(Arg::new("tag")
+                    .value_parser(value_parser!(String))
+                    .long("tag")                    
+            )    
+    )    
+    .after_help("Longer explanation to appear after the options when \
+                 displaying the help information from --help or -h")
+    .get_matches();
 
-    let journey_file = cli.journal_file.unwrap_or(PathBuf::from("todo.json"));
+    let journal_file = arg_matches.get_one::<PathBuf>("journal_file").unwrap().to_owned();
+    
+    // Comment: the following block of code works by destructuring the subcommand of the arg_matches struct
+    // Currently, in every destructuring instance, the desirable arg is extracted from the args_matches struct
+    // with to_owned() call to create an owned instance. There could be more fine-grained case-by-case consideration
+    // since some desirable arg are needed as owned instance to further be consumed e.g. adding new tasks would need to consume
+    // owned attributes in the creation of new owned Task instance anyway
+    // However, ops like removal by index or listing by tag DOES NOT SEEM TO neccessarily need to own the arg
+    // since they just need to reference the info from the arg to complete their jobs. The removal of to_owned call
+    // in this cases SEEM TO be benefitial w.r.t performance
+    match arg_matches.subcommand() {
+        Some(("list", list_args)) => {
+            let list_tag = list_args.get_one::<String>("tag")
+                                                     .to_owned();
+            task::Task::list(journal_file, list_tag)?
+        },
+        Some(("remove", remove_args)) => {
+            let remove_index = remove_args.get_one::<usize>("index")
+                                                 .unwrap()
+                                                 .to_owned();
+            task::Task::remove(journal_file, remove_index)?
+        },
+        Some(("add", add_args)) => {
+            let add_task_name = add_args.get_one::<String>("task")
+                                                .unwrap()
+                                                .to_owned();
 
-    match cli.action {
-        cli::Action::Add {task} => task::Task::add(journey_file, task)?,
-        cli::Action::Remove { index } => task::Task::remove(journey_file, index)?,
-        cli::Action::List => task::Task::list(journey_file)?,
+            let add_task_tags  = add_args.get_many::<String>("tag")
+                                            // Since the get_many call returns an Option, to process the Some() case further and 
+                                            // levae the None case as is, and_then() is used, refered as the "flatMap" equivalent
+                                            .and_then(|x|
+                                                // the Some() case is a ValuesRef struct which is an iterator resulting from
+                                                // the get_many call. Thus just needing to processing it properly and collecting
+                                                // into a collection: https://docs.rs/clap/latest/clap/parser/struct.ValuesRef.html
+                                                Some(x.map(|s| 
+                                                    s.to_owned()).collect::<Vec<_>>()));
+
+
+            task::Task::add(journal_file, add_task_name, add_task_tags)?
+        }
+        _ => unreachable!(),
     }
 
     Ok(())
--- src/task.rs
+++ src/task.rs
@@ -10,8 +10,13 @@ use std::fmt::Result as fmtResult;
 pub struct Task {
     name: String,
     state: State,
-    tags: Vec<Tag>,
-    notes: Option<String>,
+    // the tags fields can be modelled either way. If modelled with 
+    // Vec<String> then an empty Vec represents a task with no tag.
+    // If modelled with Option<Vec<String>>, then there seems to be two
+    // representations for a task with no tag, but it might be more 
+    // memory efficient if the None case is always utilised for representation
+    // tags: Vec<String>,
+    tags: Option<Vec<String>>,
     #[serde(with = "ts_seconds")]
     creted_at: DateTime<Utc>,
 }
@@ -23,10 +28,6 @@ enum State {
     Complete,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-struct Tag {
-    tag_str: String,
-}
 
 impl Display for Task {
     fn fmt (&self, f: &mut Formatter) -> fmtResult {
@@ -38,12 +39,11 @@ impl Display for Task {
 }
 
 impl Task {
-    fn new(name: String) -> Self {
+    fn new(task_name: String, task_tags: Option<Vec<String>>) -> Self {
         Task {
-            name,
+            name: task_name,
             state: State::Active,
-            tags: Vec::new(),
-            notes: None,
+            tags: task_tags,
             creted_at: Utc::now(),
         }
     }
@@ -70,9 +70,9 @@ impl Task {
     /// ```
     /// use rusty_journal_clap::task;
     /// use std::path::PathBuf;
-    /// task::Task::add(PathBuf::from("todo.json"), "new_task".to_string());
+    /// task::Task::add(PathBuf::from("todo.json"), "play".to_string(), Some(vec!["good first issue".to_string()]));
     /// ```
-    pub fn add(journal_path: PathBuf, name: String) -> ioResult<()> {
+    pub fn add(journal_path: PathBuf, name: String, tags: Option<Vec<String>>) -> ioResult<()> {
         let f = OpenOptions::new()
                                 .write(true)
                                 .create(true)
@@ -83,7 +83,7 @@ impl Task {
 
         let mut tasks = Self::_get_tasks(f)?;
 
-        let new_task = Self::new(name);
+        let new_task = Self::new(name, tags);
 
         tasks.push(new_task);
 
@@ -97,39 +97,6 @@ impl Task {
         let f = BufWriter::new(f);
 
         Self::_write_tasks(&tasks, f)?;
-
-        Ok(())
-    }
-
-    /// The method fetches the current tasks into a vec from the Json
-    /// and prints them out. Empty tasks is specifically handled within
-    /// # Examples
-    /// ```
-    /// use rusty_journal_clap::task;
-    /// use std::path::PathBuf;
-    /// task::Task::list(PathBuf::from("todo.json"));
-    /// ```    
-    pub fn list(journal_path: PathBuf) -> ioResult<()> {        
-        let f = OpenOptions::new()
-                            // write must be set first to enable create
-                            // https://doc.rust-lang.org/std/fs/struct.OpenOptions.html#method.create
-                            .write(true)
-                            .create(true)
-                            .read(true)
-                            .open(&journal_path)?;
-
-
-        let f = BufReader::new(f);
-
-        let tasks = Self::_get_tasks(f)?;
-
-        if tasks.is_empty() {
-            println!("Empty to-do list");
-        } else {
-            for task in tasks {
-                println!("{}", task);
-            }
-        }
 
         Ok(())
     }
@@ -176,6 +143,89 @@ impl Task {
         Self::_write_tasks(&tasks, f)?;
 
         Ok(())
+    }
+
+    /// The method fetches the current tasks into a vec from the Json
+    /// and prints them out. Empty tasks is specifically handled within
+    /// # Examples
+    /// ```
+    /// use rusty_journal_clap::task;
+    /// use std::path::PathBuf;
+    /// task::Task::list(PathBuf::from("todo.json"));
+    /// ```    
+    pub fn list(journal_path: PathBuf, tag: Option<&String>) -> ioResult<()> {        
+        let f = OpenOptions::new()
+                            // write must be set first to enable create
+                            // https://doc.rust-lang.org/std/fs/struct.OpenOptions.html#method.create
+                            .write(true)
+                            .create(true)
+                            .read(true)
+                            .open(&journal_path)?;
+
+
+        let f = BufReader::new(f);
+
+        let tasks = Self::_get_tasks(f)?;
+
+        if tasks.is_empty() {
+            println!("Empty to-do list");
+        } else {
+            match tag {
+                Some(tag) => {
+                    for task in tasks.iter()
+                                            .filter(|&task| task.tags.as_ref()
+                                                // the family of mapping methods (e.g. map, is_some_and) on Option type would consume the ownership of the Option
+                                                // here the task.tags is a field of the Task struct, of Option<Vec<String>> type
+                                                // If directly followed by a is_some_and call, the ownership of the field would move out of the Task struct
+                                                // which obviously is a violation as it wouldn't be allowed by the compiler either
+                                                // The as_ref method of Option type is handy here since it creates another owned Option instance to be CONSUMED
+                                                // plus with the same refereced data inside the Option for further ops 
+                                                .is_some_and(|tags| tags
+                                                    .contains(&tag))) {
+                                                        println!("{}", task);
+                                                    }
+                         
+                },
+                None => {
+                    for task in tasks {
+                        println!("{}", task);
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }    
+
+    /// This method helps with testing by clearing all the data
+    /// # Examples:
+    /// ```
+    /// use rusty_journal_clap::task;
+    /// use std::path::PathBuf;
+    /// task::Task::clear(PathBuf::from("todo.json"));
+    /// ```
+    pub fn clear(journal_path: PathBuf) -> ioResult<()> {
+        let f = OpenOptions::new()
+                                    .write(true)
+                                    .create(true)
+                                    .read(true)
+                                    .open(&journal_path)?;
+
+        let f = BufReader::new(f);
+
+        let mut tasks = Self::_get_tasks(f)?;
+
+        tasks.clear();
+
+        let f = OpenOptions::new()
+                            .truncate(true)
+                            .write(true)
+                            .open(&journal_path)?;
+
+        let f = BufWriter::new(f); 
+        Self::_write_tasks(&tasks, f)?;
+
+        Ok(())        
     }
 
 
